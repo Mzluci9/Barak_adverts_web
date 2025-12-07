cd2496433fcfebf682f405f98e649e24bc3fb219
Michael Zewdu <michaelzewdu00@gmail.com>
2025-12-07 18:08:35 +0300

    c

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,5 +1,12 @@
 'use client'
 
+/**
+ * ThemeProvider Wrapper
+ * ----------------------
+ * This component forwards all props to NextThemesProvider.
+ * Useful for global theme handling across the app.
+ */
+
 import * as React from 'react'
 import {
   ThemeProvider as NextThemesProvider,
@@ -7,5 +14,10 @@ import {
 } from 'next-themes'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  // Pass props directly to NextThemesProvider (no behavior change)
+  return (
+    <NextThemesProvider {...props}>
+      {children}
+    </NextThemesProvider>
+  )
 }
